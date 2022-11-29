d643c72c90af82d9681de0398a03a47978f1446e
ziss aja <ziss2517@gmail.com>
2022-11-29 18:01:20 +0800

    days2 countries api

--- public/main.js
+++ public/main.js
@@ -20,3 +20,26 @@ function renderTheme(text, condition) {
     <h4 class="text-sm font-semibold ml-2">${text} mode</h4>
   `;
 }
+
+// action dropdown
+const titleOutput = document.querySelector('.title-output');
+const btnAnimate = document.getElementById('btn-animate');
+const regions = document.querySelectorAll('li');
+const options = document.querySelector('.options');
+// jika dropdown di click
+btnAnimate.addEventListener('click', closeOpenDropdown);
+regions.forEach((region) => {
+  region.addEventListener('click', () => {
+    const regionTextData = region.innerText;
+    titleOutput.innerText = regionTextData;
+    // tutup dropdown jika pilih data region
+    setTimeout(closeOpenDropdown, 100);
+  });
+});
+// animate dropdown nya
+function closeOpenDropdown() {
+  btnAnimate.classList.toggle('active-btn');
+  options.classList.toggle('active-options');
+}
+
+// lanjut besok aja........
